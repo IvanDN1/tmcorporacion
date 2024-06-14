9a24ae0c9b7503b8dcb6ae68ab5ef5f9c5b45e17
IvanDN1 <ivan.ea.dn97@gmail.com>
2024-06-14 17:27:20 -0400

    actualziacion de css

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,20 +1,18 @@
 <nav class="navbar navbar-expand-lg navbar-light" >
-    <div class="container-fluid">
+    <div class="collapse navbar-collapse d-lg-flex" id="navbarsExample11">
         <a href="https://www.tmcorporacion.cl/" class="navbar-brand d-none d-md-flex align-items-center m-0 mr- 4 p-0 aabtn">
             <img src="/images/LogoATE2.png" alt="Logo de la empresa"  class="Logo mr-1" id="LogoATE" width="250px">
         </a>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarColor03">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="/">INICIO</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/nosotros">NOSOTROS</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/contact">CONTACTANOS</a>
-                </li>
-            </ul>
-        </div>
+      <ul class="navbar-nav col-lg-6 justify-content-lg-center">
+        <li class="nav-item">
+            <a class="nav-link" href="/">INICIO</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/nosotros">NOSOTROS</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/contact">CONTACTANOS</a>
+        </li>
+      </ul>
     </div>
 </nav>
