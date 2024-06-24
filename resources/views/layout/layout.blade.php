8b2812e14607a2758da8ef99b5ab7c483e63d48c
IvanDN1 <ivan.ea.dn97@gmail.com>
2024-06-24 09:51:21 -0400

    aaa

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -10,11 +10,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.scss') }}" rel="stylesheet">
     <link href="{{ asset('css/cards.css') }}" rel="stylesheet">
     <link href="{{ asset('js/app.css') }}" rel="stylesheet">
+    @vite([
+        'resources/css/app.scss',
+        'resources/js/app.js',
+       ])
     <link rel="icon" type="image/png" href="/images/IconATE.png">
     <title>{{ $title ?? 'TmCorporación' }}</title>
+
+
 </head>
 <body>
 @include('components.navbar')
