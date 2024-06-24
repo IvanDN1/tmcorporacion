0eac84666fbc0e9fd6283d891c5c3c05b5d52d73
IvanDN1 <ivan.ea.dn97@gmail.com>
2024-06-24 11:17:18 -0400

    done

--- resources/views/contact/form.blade.php
+++ resources/views/contact/form.blade.php
@@ -11,43 +11,41 @@
                     </div>
                 </div>
             </div>
-            <div class="row gx-5 justify-content-center">
-                <div class="col-lg-6 col-xl-8">
+            <div class="row gx-8 justify-content-center">
+                <div class="col-lg-8 col-xl-8">
                     <form action="{{ route('contact.send') }}" method="POST">
                     @csrf
                     <div class="form-floating mb-3">
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" name="name" placeholder="name">
-                            <div class="invalid-feedback" data-sb-feedback="name:required">se requiere un nombre.</div>
-                        </div>
+                        <input name="name" class="form-control" id="name" type="text" placeholder="Enter your name..." required>
+                        <label for="name">Full name</label>
+                        <div class="invalid-feedback" data-sb-feedback="name:required">se requiere un nombre.</div>
                     </div>
                     <br>
                     <div class="form-floating mb-3">
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" name="phone" placeholder="phone">
-                            <div class="invalid-feedback" data-sb-feedback="phone:required">se requiere un numero.</div>
-                        </div>
+                        <input name="phone" class="form-control" id="phone" type="tel" placeholder="(569) 0000-0000" required >
+                        <label for="phone">Phone number</label>
+                        <div class="invalid-feedback" data-sb-feedback="phone:required">se requiere un numero.</div>
                     </div>
                     <br>
                     <div class="form-floating mb-3">
-                        <div class="col-sm-10">
-                            <input name="email"class="form-control" id="email" type="email" placeholder="name@example.com" required>
-                            <div class="invalid-feedback" data-sb-feedback="email:required">se requiere.</div>
-                        </div>
+                        <input name="email"class="form-control" id="email" type="email" placeholder="name@example.com" required>
+                        <label for="email">Email address</label>
+                        <div class="invalid-feedback" data-sb-feedback="email:required">se requiere.</div>
                     </div>
                     <br>
                     <div class="form-floating mb-3">
-                        <div class="col-sm-10">
-                            <input class="form-control"  name="subject" id="subject" type="text" placeholder="Enter your subject..." required>
-                            <div class="invalid-feedback" data-sb-feedback="subject:required">A phone is required.</div>
-                        </div>
+                        <input class="form-control"  name="subject" id="subject" type="text" placeholder="Enter your subject..." required>
+                        <label for="subject">Subject</label>
+                        <div class="invalid-feedback" data-sb-feedback="subject:required">A phone is required.</div>
                     </div>
                     <br>
                     <div class="form-floating mb-3">
-                        <div class=" message mb-3 ">
-                            <textarea class="form-control" name="message"type="text" placeholder="Enter your message here..." style="height: 10rem" required></textarea>
-                        </div>
+                        <textarea name="message" class="form-control" id="message" type="text" placeholder="Enter your message here..." style="height: 10rem" required></textarea>
+                        <label for="message">Message</label>
+                    </div>
+                    <div class="d-grid">
                         <button class="btn btn-outline-success btn-lg">Submit</button>
+                    </div>
                     </div>
                 </form>
             </div>
